613c02520ea63060ff450289a31c7de44f172dbb
mac <castromac6367@gmail.com>
2025-08-27 11:50:54 -0300

    media de aluno

---
+++ semana1/mediaAluno.py
@@ -0,0 +1,8 @@
+nota = float(input("Digite a nota do aluno: "))
+
+if nota >= 7:
+    print("Parabéns! Você está aprovado.")
+elif nota >=5:
+    print("ReVocê está de recuperação.cuperação")    
+else:
+    print("Infelizmente, você está reprovado.")

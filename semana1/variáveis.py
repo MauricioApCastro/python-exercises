ff7739d386796d37c48fe9c38bb3a361382a5d21
mac <castromac6367@gmail.com>
2025-08-27 09:24:42 -0300

    variáveis

---
+++ semana1/variáveis.py
@@ -0,0 +1,20 @@
+#entrada de dados
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+
+#processamento
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2 
+divisao = num1 / num2
+potencia = num1 ** num2
+
+#saida de dados
+print("A soma é:", soma)
+print("A subtração é:", subtracao)
+print("A multiplicação é:", multiplicacao)
+print("A divisão é:", divisao)
+print("A potência é:", potencia)
+ 
+ 

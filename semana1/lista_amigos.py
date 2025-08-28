52dfc290e26193596613ea7c0b9f40bf5f9b2678
mac <castromac6367@gmail.com>
2025-08-28 08:32:55 -0300

    mostra lista de amigos

---
+++ semana1/lista_amigos.py
@@ -0,0 +1,18 @@
+#inicia lista vazia
+lista = []
+
+for i in range(3):
+    nome = input(f"Digite o nome do amigo {i+1}: ")
+    lista.append(nome)
+
+
+print("Lista de amigos:", lista)
+
+
+
+
+
+
+
+
+

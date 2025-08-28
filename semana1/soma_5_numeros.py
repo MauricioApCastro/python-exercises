e658a1a4fea4678f90761fb0e3a9411b68b37607
Mauricio <castromac6367@gmail.com>
2025-08-28 13:35:58 -0300

    exercícios

---
+++ semana1/soma_5_numeros.py
@@ -0,0 +1,9 @@
+
+soma_numero = 0
+
+for i in range (1,6):
+    
+    numero = int(input(f"Digite o {i}º número: "))
+    soma_numero +=  numero
+    
+print(f"A soma dos números é: {soma_numero}")
--- semana1/tabuada_do_5.py
+++ semana1/tabuada_do_5.py
@@ -1,2 +1,2 @@
 for i in range(0,10):
-    print(f"5 X {i} = {5*i}")
+    print(f"5 X {i} = {5*i}º")

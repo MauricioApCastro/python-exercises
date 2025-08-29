821cf1887689fdf3dcdfc57d20e14866c449ce21
Mauricio <castromac6367@gmail.com>
2025-08-29 12:38:22 -0300

    refatoração de códigos

--- semana1/soma_5_numeros.py
+++ semana1/soma_5_numeros.py
@@ -1,9 +1,11 @@
 
-soma_numero = 0
+lista_numeros = []
 
-for i in range (1,6):
-    
+for i in range (1,6):   
     numero = int(input(f"Digite o {i}º número: "))
-    soma_numero +=  numero
+    lista_numeros.append(numero)
+
+media = sum(lista_numeros) / len(lista_numeros)
+
     
-print(f"A soma dos números é: {soma_numero}")
+print(f"A soma dos números é: {media}")

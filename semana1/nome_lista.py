7fae003af33de237a3d2d707605241b39d190856
Mauricio <castromac6367@gmail.com>
2025-08-29 08:08:13 -0300

    refatoração de nome_lista

--- semana1/nome_lista.py
+++ semana1/nome_lista.py
@@ -1,11 +1,15 @@
+#inicia uma lista vazia
 lista_nomes = []
 
+#pede e armazena 5 nomes na lista
 for i in range(1, 6):
     nome = input(f"Digite o nome {i}: ")
     lista_nomes.append(nome)
 
+#pede e armazena nome para busca
 nome_procurado = input("Digite um nome para verificar: ")
 
+#verifica se o nome está na lista
 if nome_procurado in lista_nomes:
     print(f"O nome {nome_procurado} está na lista")
 else:

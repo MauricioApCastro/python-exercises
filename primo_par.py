bf986a1c33544fa2be97af79a4f6f06563daea42
mauricio <castromac6367@gmail.com>
2025-08-30 07:49:36 -0300

    numero primo ou par versão otimizada

--- primo_par.py
+++ primo_par.py
@@ -1,16 +1,6 @@
-lista_resultado = []
 num = int(input("Digite um número: "))
 
-for i in range(1, num + 1):   # percorre de 1 até o número
-    res = (num % i == 0)      # True se for divisor
-    lista_resultado.append(res)
-
-# Um número primo só tem 2 divisores: 1 e ele mesmo
-if lista_resultado.count(True) == 2:
+if num > 1 and all(num % i != 0 for i in range(2, num)):
     print(f"{num} é primo")
 else:
     print(f"{num} não é primo")
-
-print(lista_resultado)  # só para você ver como fica a lista
-
-

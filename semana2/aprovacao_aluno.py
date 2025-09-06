f0d522bd357fc212d81807d190b06aad1221c432
mauricio <castromac6367@gmail.com>
2025-09-06 08:55:55 -0300

    sistema de notas e estatísticas

--- semana2/aprovacao_aluno.py
+++ semana2/aprovacao_aluno.py
@@ -4,18 +4,34 @@ nome = input("Qual o seu nome?: ")
 qtde_notas =int(input("Quantas notas você vai digitar: "))
 
 for i in range(1,qtde_notas + 1):
-    nota = float(input(f"Digite a {1} nota: "))
+    nota = float(input(f"Digite a {i}ª nota: "))
     lista_notas.append(nota)
 
 
 def calcular_media(lista):
 
-    return
-    sum(lista_notas) / len(lista_notas)
+    return sum(lista) / len(lista)
+
+def aprovacao(media):
+    if media >= 7 :
+        return "Aprovado"
+    elif media >= 5:
+        return "recuperação"
+    else :
+        return "reprovado"
+
+def par_impar(lista):   
+        pares = [n for n in lista_notas if n % 2 == 0]
+        impares = [n for n in lista_notas if n % 2 != 0]
+        return pares, impares
+     
 
 media = calcular_media(lista_notas)
+status = aprovacao(media)
+pares, impares = par_impar(lista_notas)
 
-
-print(f"Nome do aluno {nome}")
-print(f"Suas notas são: {media:.2f}")
+print(f"Nome do aluno: {nome}")
+print(f"Suas notas são: {lista_notas}")
 print(f"Sua média final é {media}")
+print(f"Vocé está {status}")
+print(f"A lista de notas pares é {pares} e a ímpares é {impares}")
